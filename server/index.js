6dad3f4c5547e09aa00f0296120acb0559e3e473
Amina Haq <haq.3@wright.edu>
2023-11-14 19:23:10 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const db = require("./config/db");
 const cors = require("cors");
 const app = express();
-const PORT = 3002;
+const PORT = process.env.PORT || 3002;
 
 app.use(cors());
 app.use(express.json());
